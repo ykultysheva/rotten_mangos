e851a77d8aa78f1d179077dc797e14cba79ced96
Yana Kultysheva <ykultysheva@gmail.com>
2016-07-18 13:43:20 -0400

    Initial Commit

---
+++ RottenMangos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenMangos
+//
+//  Created by Yana Kultysheva on 2016-07-18.
+//  Copyright © 2016 Yana Kultysheva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
